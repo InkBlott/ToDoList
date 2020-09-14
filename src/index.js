cbc3eca4a30b6cda027c113214df83d78e1349fe
Ink <bmantas@tutanota.com>
2020-09-14 11:18:33 +0300

    ToDo app using modules and webpack,  saving to localStorage.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {toDoFactory} from './toDo.js'
+
+import {displayProject, projAdder} from './display.js'
+
+const addProjBtn = document.getElementById('addProjBtn');
+
+
+
+const addProjectBtn = document.getElementById('addProjBtn');
+addProjectBtn.onclick = projAdder;
+
+
+
+
+
